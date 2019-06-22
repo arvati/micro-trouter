adac338a55b79c3d46adfae8a822c1254fd2d7bf
Ademar Arvati Filho <arvati@hotmail.com>
2019-06-22 01:09:38 -0300

    changing
    Signed-off-by: Ademar Arvati Filho <arvati@hotmail.com>

--- example/example.js
+++ example/example.js
@@ -1,18 +1,24 @@
 const microTrouter = require('../lib');
 
-const one =  async (req, res) => {
-    req.hello = 'world';
-}
 
-const two =  async (req, res) => {
-    req.foo = '...needs better demo 😔';
-}
+
+const first = next => async (request, response, ...args) => {
+    request.hello = 'world';
+    return next(request, response, ...args);
+};
+
+const second = next => (request, response, ...args) => {
+    request.foo = '...needs better demo 😔';
+    //let oneMore = 'another one'
+    //args.push(oneMore);
+    return next(request, response, ...args);
+};
 
 module.exports = microTrouter()
-    .use(one, two)
-    .get('/users/:id', async (req, res) => {
-        console.log(`~> Hello, ${req.hello}`);
+    .use(first, second)
+    .get('/users/:id', async (request, response) => {
+        console.log(`~> Hello, ${request.hello}`);
         //res.end(`User: ${req.params.id}`);
-        return `User: ${req.params.id}`
+        return `User: ${request.params.id}`
     })
-    .handler
+    .compose
