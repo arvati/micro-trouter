bf7dd022bf4bbf0ab6454035ad65795275032f21
Ademar Arvati Filho <arvati@hotmail.com>
2019-06-28 22:12:45 -0300

    final before testing
    Signed-off-by: Ademar Arvati Filho <arvati@hotmail.com>

--- example/example.js
+++ example/example.js
@@ -1,36 +1,36 @@
 const microTrouter = require('../lib');
 
-const one =  async (req, res) => {
-    req.hello = 'world';
+const one =  async (request, response) => {
+    request.hello = 'world!';
 }
 
-const two =  async (req, res) => {
-    req.foo = '...needs better demo 😔';
+const two =  async (request, response) => {
+    request.foo = '...needs better demo 😔 !!';
 }
 
 const first = next => async (request, response, ...args) => {
-    request.hello = 'world';
+    request.hello = 'world?';
     return next(request, response, ...args);
 };
 
-const second = next => (request, response, ...args) => {
-    request.foo = '...needs better demo 😔';
+const second = next => async (request, response, ...args) => {
+    request.foo = '...needs better demo 😔 ??';
     //let oneMore = 'another one'
     //args.push(oneMore);
     return next(request, response, ...args);
 };
 
 module.exports = microTrouter()
-    .use(one, two)
-    .compose(first, second)
-    .get('/users/:id', async (request, response) => {
+    .compose("demo", first, second)
+    .use("users", one, two)
+    .get('users/:id', async (request, response) => {
         console.log(`~> Hello, ${request.hello}`);
         //res.end(`User: ${req.params.id}`);
         return `User: ${request.params.id}`
     })
-    .get('/users/:id', async (request, response) => {
+    .get('demo/:id', async (request, response) => {
         console.log(`~> Really, ${request.foo}`);
         //res.end(`User: ${req.params.id}`);
-        return `User: ${request.params.id}`
+        return `Demo: ${request.params.id}`
     })
     .handle
